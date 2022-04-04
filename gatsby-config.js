9fd7abeb921ba27a29e0ef59903657640117cf86
Dustin Thurston <dthurston1996@gmail.com>
2022-04-03 20:17:10 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-1CF5H7E26H"
+    }
+  }]
+};
