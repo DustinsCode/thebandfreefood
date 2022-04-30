12362b01faba6b1fc26230cfc7d57a068f7de9e3
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2022-04-30 19:38:35 -0400

    Added links to navbar

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import * as styles from '../styles/navbarDesktop.module.scss'
-import { StaticImage } from 'gatsby-plugin-image'
+
 
 function NavItem(props) {
     return (
         <a href={props.link} className={styles.navbarDesktop}>
             <div>{props.text}</div>
-            <StaticImage className={styles.navbarDesktop} src={`../images/" ${props.icon}`} alt={props.alt} />
+            <nav className={styles.navbarDesktop}>
+                {props.icon}
+            </nav>
         </a>
     )
 }
--- src/components/navbarDesktop.js
+++ src/components/navbarDesktop.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import * as styles from '../styles/navbarDesktop.module.scss'
 import NavItem from './NavItem'
+import { FaHome } from 'react-icons/fa';
+import { FaHeadphones } from 'react-icons/fa';
+import { FaFrog } from 'react-icons/fa';
 
 function NavbarDesktop(props) {
+
+    const iconSize = 50
+
     return (
         <navbar className={styles.NavbarDesktop}>
             <NavItem 
                 className={styles.NavbarDesktop}
-                text={"Home"}
-                icon={'house-solid.svg'}
-                alt={'home'}
-             />
+                text = {"Home"}
+                icon = {<FaHome size={iconSize}/>}
+                link = {"https://thebandfreefoodmain.gatsbyjs.io/"}
+            />
+
             <NavItem 
                 className={styles.NavbarDesktop}
                 text={"Tunes"}
+                icon = {<FaHeadphones size={iconSize}/>}
+                link="https://open.spotify.com/artist/19yT2G1cULygYhO2ijQiPi?si=_HP6KXZYRuuVxqjeyA0wKw"
              />
+
             <NavItem 
                 className={styles.NavbarDesktop}
                 text={"Frog"}
+                icon = {<FaFrog size={iconSize}/>}
+                link = {"https://en.pimg.jp/048/818/553/1/48818553.jpg"}
              />
+             
         </navbar>
     )
 }
